3cc11b00e29937fa198abe74d35121bb6da9fc8f
Anthony Richards <atrichards611@gmail.com>
2016-02-26 08:58:17 -0500

    Added ability to name player, and help command Made status static, removed the command, now prints after every command No more negative gold

--- commands.py
+++ commands.py
@@ -2,7 +2,8 @@
 class commands(object):
 
     def quit(self):
-        confirm = raw_input("Are you sure you want to quit (Yes/No)? Make sure you save!")
+        allcmd.append(quit)
+        confirm = raw_input("Are you sure you want to quit (Yes/No)? Make sure you save! ")
         confirm = confirm.lower()
         if confirm == 'yes':
             print "Game over"
@@ -11,15 +12,23 @@ class commands(object):
             print ("We all make mistakes.")
 
     def status(self, player):
+        print ("*"*10)
+        print str(player.name)
+        print ("Status:")
         print ("Current HP: " + str(player.hp))
         print ("Current gold: " + str(player.gold))
+        print ("*"*10)
 
     def hit(self, player):
         player.hp -= 5
-        print ("Oh shit you got hit.")
+        print ("You got hit.")
         print ("You have {0} health left.").format(player.hp)
 
     def rob(self, player):
-        player.gold -= 1
-        print ("Oh shit you got robbed.")
+        player.gold -= 5
+        print ("You payed up.")
         print ("You have {0} gold left.").format(player.gold)
+
+    def help(self):
+        print ("The following commands are useable: ")
+        print ("Help, Hit, Quit, Pay")
--- game.py
+++ game.py
@@ -5,14 +5,16 @@ from commands import commands
 import os
 
 class character(object):
-    def __init__(self, gold, hp):
+    def __init__(self, gold, hp, name):
         self.hp = hp
         self.gold = gold
-        self.name = ""
+        self.name = name
 
 def game():
     os.system('clear')
-    player = character(10, 20)
+    name = raw_input("Please enter your name: ")
+    print ("Hello {0} and thank you for playing. Here's 10 gold to start.").format(name)
+    player = character(10, 20, name)
     instance = commands()
     game_over = False
     while not game_over:
@@ -20,18 +22,21 @@ def game():
             print ("Oh no, you are dead. Game Over.")
             game_over = True
         else:
+            instance.status(player)
             command = raw_input("Please enter a command: ")
             command = command.lower()
             if command == 'quit':
                 instance.quit()
                 game_over = True
-            elif command == ('status'):
-                instance.status(player)
-            elif command == ('talk shit get hit'): # Debug to test taking damage
+            elif command == ('hit'): # Debug to test taking damage
                 instance.hit(player)
-            elif command == ('look away lose some pay'): # Debug to test losing gold
+            elif command == ('pay'): # Debug to test losing gold
                 if player.gold == 0:
-                    print ("You're broke, you can't lose anymore money!")
+                    print ("You're broke.")
+                else:
+                    instance.rob(player)
+            elif command == ('help'):
+                instance.help()
             else:
                 print ("That is not a valid command. Try again.")
                 game_over = False
