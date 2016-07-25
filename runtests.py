6a385b4b602447d98e127dda0d3d3cfa1aa07d1a
Charles <cshapleigh@datto.com>
2016-07-24 21:55:45 -0400

    Create test runner for ease of life

---
+++ runtests.py
@@ -0,0 +1,16 @@
+import glob
+import unittest
+
+testmodules = [
+    'tests.unit.inventorytest',
+    'tests.unit.charactertest',
+    # add modules here as you write them
+    ]
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+        # load  all tests in module
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
